c5d31672734d1d45334cc723c91df949d398c535
DivyaD1108 <divya.minalapuram@gmail.com>
2024-06-10 12:16:27 +0530

    Delete coreApi/coreApi directory

--- coreApi/coreApi/DataModels/GlobalConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace coreApi.DataModels
-{
-    public class GlobalConstants
-    {
-        public static string Success_Result = "Success";
-        public static string Failure_Result = "Failure";
-    }
-}
